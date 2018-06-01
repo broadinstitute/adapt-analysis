96f5ebcb3002cf93d44c21f6fcad4c4365fa0b93
Hayden Metsky <hmetsky@gmail.com>
2018-06-01 14:16:39 -0400

    Add table giving year for each N subtype sequence
    This adds a script to map sequence name (in a particular format) to the year of
    the sequence, printing out a tsv. This commit has the output of that script,
    run on all N subtype sequences (Hstar), saved to
    2k8_Hstar_aligned_amplicon.seq-with-year.tsv.  The script output 12 'unknown's
    (sequences for which it could not determine the year). I manually replaced
    'unknown' with the year in the file for those 12 sequences, and also manually
    fixed incorrect years that were output (well outside the 2008-2018 range) for 2
    sequences.

---
+++ iav-subtype-id/scripts/map_seq_name_to_year.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Args:
+#  1: filename listing a sequence on each line
+
+# This assumes the year can be found in the pattern
+#  '/[year]_[4 digits]' where [year] is 4 digits and [4 digits] is any 4 digits
+
+while read -r seqname; do
+    # Print the sequence name in the first column
+    echo -ne "$seqname\t"
+
+    # Extract the year and print it in the second column (or 'unknown' if not found)
+    # This will pull out [4 digits]_[4 digits] and only take the first [4 digits] as the
+    # year
+    # Use 'head -1' to only print the first match in a line (there should only be one match,
+    # but in rare exceptions there may be more than one, in which case we may output an
+    # incorrect year)
+    (echo "$seqname" | grep -Po '/\d{4}_\d{4}' || printf "unknown\n") | head -1 | sed 's/\///' | awk -F'_' '{print $1}'
+done < "$1"
