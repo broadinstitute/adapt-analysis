58bc526c5221dd55e1ca8b3245816322eec5bdbf
Hayden Metsky <hmetsky@gmail.com>
2018-06-11 18:15:43 -0400

    Add coverage of H subtype design on v2 design
    This uses the latest guide design (2k8v2_H_crRNAs.fasta) for H
    subtypes, which seeks more desired coverage for more recent years.
    The commit includes information about the coverage of the design as
    determined by alignment with bwa.

---
+++ iav-subtype-id/diff-id-runs/verify/H-subtype-id/calculate_H_coverage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../../scripts/calculate_subtype_coverage.sh H data/2k8v2_H_crRNAs.fasta data/iav-seqs tmp out
---
+++ iav-subtype-id/diff-id-runs/verify/H-subtype-id/calculate_H_coverage_per_year.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+../../../scripts/calculate_subtype_coverage_per_year.sh H data/iav-seqs ../../data/iav-seqs-years/2k8_Nstar.acc-with-year.tsv tmp out
