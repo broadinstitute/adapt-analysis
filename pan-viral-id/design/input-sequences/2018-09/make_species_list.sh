11d1139d7a304dceb4d3446b4ed21c1b1393da42
Hayden Metsky <hmetsky@gmail.com>
2018-09-10 18:25:42 -0400

    Remake species list for pan-viral guide design using modifications to viral accession list
    This remakes the list of species to include in the pan-viral
    guide design, using modifications (changes and additions) to
    the 2018-09-05 pull of the viral accession list. (See immediate
    prior commits for these changes.)

--- pan-viral-id/design/input-sequences/2018-09/make_species_list.sh
+++ pan-viral-id/design/input-sequences/2018-09/make_species_list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../download_seqs.py make-species-list taxid10239.nbr.20180905.txt -o species-list.tsv --human-host-lineages-to-add human-host-lineages-to-add.txt
+python ../download_seqs.py make-species-list taxid10239.nbr.20180905.modified.txt -o species-list.tsv --human-host-lineages-to-add human-host-lineages-to-add.txt
