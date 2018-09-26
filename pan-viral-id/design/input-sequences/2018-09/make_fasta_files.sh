ab7de8df3d0c26263dbf07e3694786c7795b3423
Hayden Metsky <hmetsky@gmail.com>
2018-09-26 11:47:08 -0400

    Update make_fasta_files.sh to provide Influenza A/B/C sequences when making fasta files
    The output of this script, the fasta files, are ignored from
    the repo in the .gitignore.

--- pan-viral-id/design/input-sequences/2018-09/make_fasta_files.sh
+++ pan-viral-id/design/input-sequences/2018-09/make_fasta_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../download_seqs.py make-fasta-files taxid10239.nbr.20180905.modified.txt -o fasta-files --human-host-lineages-to-add human-host-lineages-to-add.txt
+python ../download_seqs.py make-fasta-files taxid10239.nbr.20180905.modified.txt -o fasta-files --human-host-lineages-to-add human-host-lineages-to-add.txt --influenza-seqs human-influenza-ABC-seqs.tsv
