fb1cb810f0338fe5a23cd9fbdbe21cdf09b864be
Hayden Metsky <hmetsky@gmail.com>
2018-10-21 10:55:22 -0400

    Make changes to pan-viral design input based on alignment issues
    This makes several changes to the input accessions used for
    pan-viral guide design (2018-09) based on issues in the sequences
    that Cameron noticed when making alignments from the fasta files.
    
    The changes to the accession list (taxid10239.nbr.20180905.modified.txt) are:
      - Remove all Giant panda anellovirus sequences; these do not
        appear to infect humans
      - Remove 1010 other sequences for various reasons (e.g., permuted
        genome)
      - Change the segment of KU978811.1 (an LCMV sequence) from S to L
    
    This also adds a list of sequences -- seqs-to-reverse-complement.txt --
    for which the reverse complement should be used (i.e., the sequence
    was in the wrong orientation for alignment).
    
    See
      https://docs.google.com/spreadsheets/d/1D0wLiIVuvjz0Dvp67r2CkSe-La0cqBxN4MtjQC_vTEQ/edit
    for more details.

--- pan-viral-id/design/input-sequences/2018-09/make_fasta_files.sh
+++ pan-viral-id/design/input-sequences/2018-09/make_fasta_files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../download_seqs.py make-fasta-files taxid10239.nbr.20180905.modified.txt -o fasta-files --human-host-lineages-to-add human-host-lineages-to-add.txt --influenza-seqs human-influenza-ABC-seqs.tsv
+python ../download_seqs.py make-fasta-files taxid10239.nbr.20180905.modified.txt -o fasta-files --human-host-lineages-to-add human-host-lineages-to-add.txt --reverse-complement-accessions seqs-to-reverse-complement.txt --influenza-seqs human-influenza-ABC-seqs.tsv
