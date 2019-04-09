2ddf6ec0efdac5e2a3290e5685f12b88c641c03d
Hayden Metsky <hmetsky@gmail.com>
2019-04-09 17:38:55 -0400

    For cross-validation coverages, take median of top 20 in each design

--- design-analyses/cross-validation/compile_coverages.sh
+++ design-analyses/cross-validation/compile_coverages.sh
@@ -5,9 +5,9 @@
 # Author: Hayden Metsky <hayden@mit.edu>
 
 
-# Let the coverage of a design be the mean of the top TOPN targets
+# Let the coverage of a design be the median of the top TOPN targets
 # in the design
-TOPN=10
+TOPN=20
 
 for dir in $(ls -1 . | grep 'tax-'); do
     design_name=$(echo "$dir" | sed -s 's/tax-//')
@@ -18,7 +18,8 @@ for dir in $(ls -1 . | grep 'tax-'); do
 
     # Compute a coverage value for each resampling
     for design_file in $(ls -1 tax-${design_name}/designs/coverages/design-*.txt); do
-        topn_mean=$(cat $design_file | tail -n +2 | awk -F '\t' -v topn="$TOPN" '$1<=topn {s+=$5} END {print s/topn}')
-        echo "$topn_mean" >> tax-${design_name}/coverage-against-test.distribution.txt
+        # Last awk command takes median; everything before extracts/sorts the top TOPN coverage values
+        topn_median=$(cat $design_file | tail -n +2 | awk -F '\t' -v topn="$TOPN" '$1<=topn {print $5}' | sort -g | awk '{ covg[NR] = $1 } END { if (NR % 2) { print covg[(NR + 1) / 2] } else { print (covg[(NR / 2)] + covg[(NR / 2) + 1]) / 2.0 } }')
+        echo "$topn_median" >> tax-${design_name}/coverage-against-test.distribution.txt
     done
 done
