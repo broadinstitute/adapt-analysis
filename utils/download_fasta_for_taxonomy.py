ac4cc4564fc6f4200e761cbed13062fc37dd97e3
Hayden Metsky <hmetsky@gmail.com>
2019-05-17 18:20:12 -0400

    Add utility script to download FASTA for a taxonomic ID

---
+++ utils/download_fasta_for_taxonomy.py
@@ -0,0 +1,38 @@
+"""Fetch accessions for taxonomic ID and a FASTA of sequences for those.
+"""
+
+import argparse
+
+import ncbi
+import seq_io
+
+__author__ = 'Hayden Metsky <hayden@mit.edu>'
+
+
+def main(args):
+    # Fetch accessions for tax_id
+    segment = None if args.segment == 'None' else args.segment
+    accessions = ncbi.fetch_neighbors_acc(args.tax_id, segment)
+
+    # Fetch fasta for these accessions (a tempfile)
+    fasta_tf = ncbi.fetch_fastas(accessions)
+
+    # Print out the contents of the FASTA
+    with open(fasta_tf.name) as f:
+        for line in f:
+            print(line.rstrip())
+
+    # Close the tempfile
+    fasta_tf.close()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('tax_id', type=int,
+            help="NCBI taxonomid ID")
+    parser.add_argument('segment',
+            help="Segment of taxonomy (or 'None' if unsegmented)")
+
+    args = parser.parse_args()
+    main(args)
