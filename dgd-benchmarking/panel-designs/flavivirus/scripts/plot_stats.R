4e3e743b7c2a026d0afa90183aa3778e6a36f3ae
Hayden Metsky <hmetsky@gmail.com>
2019-11-27 11:56:53 -0500

    Add new plots to visualize comparison of flavivirus panel to non-specific designs

--- dgd-benchmarking/panel-designs/flavivirus/scripts/plot_stats.R
+++ dgd-benchmarking/panel-designs/flavivirus/scripts/plot_stats.R
@@ -11,11 +11,13 @@ library(gridExtra)
 library(viridis)
 library(ggforce)
 
-summary.fn <- "out/summary.tsv"
+summary.fn <- "summary/summary.tsv"
 summary <- data.frame(read.table(summary.fn, header=TRUE, sep="\t"))
 
 # Replace '_' in column names with '.'
 names(summary) <- gsub("_", ".", names(summary))
+
+summary$name <- factor(summary$name)
 
 # Pull out taxa with a comparison to the non-speific design
 summary.ns <- summary[!is.na(summary$nonspecific.cost), ]
@@ -28,50 +30,58 @@ summary.ns$target.len.diff <- summary.ns$target.len - summary.ns$nonspecific.tar
 summary.ns$num.primers.total.diff <- summary.ns$num.primers.total - summary.ns$nonspecific.num.primers.total
 summary.ns$num.guides.diff <- summary.ns$num.guides - summary.ns$nonspecific.num.guides
 
-# Add dummy x-axis variable for below
-summary.ns$x.const <- 1
-summary.ns$x.const <- factor(summary.ns$x.const)
+# Determine non-specific and specific colors
+nonspecific.pt.color <- viridis(2)[1]
+specific.pt.color <- viridis(2)[2]
 
-# Plot difference in cost
-p <- ggplot(summary.ns, aes(x=x.const, y=cost.diff))
-p <- p + geom_jitter(width=0.4, height=0, size=5)
-p <- p + ylab("Difference in cost") + ylim(-0.5, 1.0)
-p <- p + scale_x_discrete(breaks=NULL)  # remove x-axis; it's meaningless here
+# The dengue cost is so much larger than the others and throws off the
+# cost plot; bring down values > 3 by 2
+summary.ns$cost <- ifelse(summary.ns$cost<=3, summary.ns$cost, ifelse(summary.ns$cost<5,NA,summary.ns$cost-2))
+summary.ns$nonspecific.cost <- ifelse(summary.ns$nonspecific.cost<=3, summary.ns$nonspecific.cost, ifelse(summary.ns$nonspecific.cost<5,NA,summary.ns$nonspecific.cost-2))
+
+# Plot non-specific and specific costs
+p <- ggplot(summary.ns, aes(x=name))
+p <- p + geom_point(aes(y=nonspecific.cost), color=nonspecific.pt.color)
+p <- p + geom_point(aes(y=cost), color=specific.pt.color)
+p <- p + ylab("Cost") + xlab("Species")
+p <- p + scale_y_continuous(breaks=c(2.5,3,3.5,4), labels=c(2.5,3,5.5,6), limits=c(2.5,4))  # accommodate high dengue cost value by inserting break
 p <- p + theme_bw(base_size=18) # bw & larger font sizes
 p <- p + theme(panel.grid.minor=element_blank(), # leave out minor grid
-               aspect.ratio=2,  # vertically stretched
-               axis.title.x=element_blank())    # no x-axis title
-ggsave("plots/cost-diff.pdf", p, width=8, height=8, useDingbats=FALSE)
+               aspect.ratio=1,  # square
+               axis.text.x=element_text(angle=45, vjust=1, hjust=1)) # 45 degree x-axis label
+ggsave("plots/costs.pdf", p, width=8, height=8, useDingbats=FALSE)
 
-# Plot difference in target length
-p <- ggplot(summary.ns, aes(x=x.const, y=target.len.diff))
-p <- p + geom_jitter(width=0.4, height=0, size=5)
-p <- p + ylab("Difference in target length (nt)") + ylim(-5, 30)
-p <- p + scale_x_discrete(breaks=NULL)  # remove x-axis; it's meaningless here
+# Plot non-specific and specific target lengths
+p <- ggplot(summary.ns, aes(x=name))
+p <- p + geom_point(aes(y=nonspecific.target.len), color=nonspecific.pt.color)
+p <- p + geom_point(aes(y=target.len), color=specific.pt.color)
+p <- p + scale_y_continuous(breaks=c(80,90,100,110,120), limits=c(80,120))
+p <- p + ylab("Target length") + xlab("Species")
 p <- p + theme_bw(base_size=18) # bw & larger font sizes
 p <- p + theme(panel.grid.minor=element_blank(), # leave out minor grid
-               aspect.ratio=2,  # vertically stretched
-               axis.title.x=element_blank())    # no x-axis title
-ggsave("plots/target-len-diff.pdf", p, width=8, height=8, useDingbats=FALSE)
+               aspect.ratio=1,  # square
+               axis.text.x=element_text(angle=45, vjust=1, hjust=1)) # 45 degree x-axis label
+ggsave("plots/target-len.pdf", p, width=8, height=8, useDingbats=FALSE)
 
-# Plot difference in total number of primers
-p <- ggplot(summary.ns, aes(x=x.const, y=num.primers.total.diff))
-p <- p + geom_jitter(width=0.4, height=0, size=5)
-p <- p + ylab("Difference in total number of primers") + ylim(-1, 2)
-p <- p + scale_x_discrete(breaks=NULL)  # remove x-axis; it's meaningless here
+# Plot non-specific and specific total number of primers
+p <- ggplot(summary.ns, aes(x=name))
+p <- p + geom_point(aes(y=nonspecific.num.primers.total), color=nonspecific.pt.color)
+p <- p + geom_point(aes(y=num.primers.total), color=specific.pt.color)
+p <- p + ylab("Total number of primers") + xlab("Species")
 p <- p + theme_bw(base_size=18) # bw & larger font sizes
 p <- p + theme(panel.grid.minor=element_blank(), # leave out minor grid
-               aspect.ratio=2,  # vertically stretched
-               axis.title.x=element_blank())    # no x-axis title
-ggsave("plots/total-num-primers-diff.pdf", p, width=8, height=8, useDingbats=FALSE)
+               aspect.ratio=1,  # square
+               axis.text.x=element_text(angle=45, vjust=1, hjust=1)) # 45 degree x-axis label
+ggsave("plots/total-num-primers.pdf", p, width=8, height=8, useDingbats=FALSE)
 
-# Plot difference in number of guides
-p <- ggplot(summary.ns, aes(x=x.const, y=num.guides.diff))
-p <- p + geom_jitter(width=0.4, height=0, size=5)
-p <- p + ylab("Difference in number of guides") + ylim(-1, 2)
-p <- p + scale_x_discrete(breaks=NULL)  # remove x-axis; it's meaningless here
+# Plot non-specific and specific number of guides
+p <- ggplot(summary.ns, aes(x=name))
+p <- p + geom_point(aes(y=nonspecific.num.guides), color=nonspecific.pt.color)
+p <- p + geom_point(aes(y=num.guides), color=specific.pt.color)
+p <- p + scale_y_continuous(breaks=c(1,2,3,4,5,6))
+p <- p + ylab("Number of guides") + xlab("Species")
 p <- p + theme_bw(base_size=18) # bw & larger font sizes
 p <- p + theme(panel.grid.minor=element_blank(), # leave out minor grid
-               aspect.ratio=2,  # vertically stretched
-               axis.title.x=element_blank())    # no x-axis title
-ggsave("plots/num-guides-diff.pdf", p, width=8, height=8, useDingbats=FALSE)
+               aspect.ratio=1,  # square
+               axis.text.x=element_text(angle=45, vjust=1, hjust=1)) # 45 degree x-axis label
+ggsave("plots/num-guides.pdf", p, width=8, height=8, useDingbats=FALSE)
