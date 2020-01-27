12f10a17acf257a11351b3bd7d31745c024cb600
Hayden Metsky <hmetsky@gmail.com>
2020-01-27 12:26:26 -0500

    Add designs of human coronaviruses (incl. nCoV) specific against all coronaviruses
    This includes a few changes from prior designs:
      - 6 new genomes in the input
      - Enforces bounds on GC content in primers
      - Outputs more than 10 designs

---
+++ misc-designs/ncov/specific/design-human-coronaviruses/specific_all-coronaviruses_idm3_idfrac0/README.txt
@@ -0,0 +1,2 @@
+This designs for the 6 human-infecting coronavirus species with >= 10 genomes (including nCoV), such that each design is specific against the other 47 coronavirus species.
+This uses `--id-m 3 --id-frac 0` to enforce specificity.
---
+++ misc-designs/ncov/specific/design-human-coronaviruses/specific_all-coronaviruses_idm4/run_specific.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Design specific panel.
+
+# Set input/output
+IN_TSV="input/coronaviruses.tsv"
+OUT_TSV_DIR="out/designs"
+
+MANUAL_SEQ_INPUT="input/manual-fasta-specify.tsv"
+
+# Set variables for design
+PREP_MEMOIZE_DIR="/ebs/dgd-analysis/prep-memoize-dir"
+MAFFT_PATH="/home/hayden/viral-ngs/viral-ngs-etc/conda-env/bin/mafft"
+CLUSTER_THRESHOLD=0.20
+ARG_GL="28"
+ARG_GM="1"
+ARG_GP="0.99"
+ARG_PL="30"
+ARG_PM="1"
+ARG_PP="0.99"
+ARG_MAXPRIMERSATSITE="5"
+ARG_MAXTARGETLENGTH="250"
+ARG_COSTFNWEIGHTS="0.6667 0.2222 0.1111"
+ARG_BESTNTARGETS="100"
+
+# Set a predictive model
+# This is from commit da10963 of my adapt-seq-design repo
+PREDICTIVE_MODEL="/home/hayden/adapt-seq-design/models/predictor_exp-and-pos_regress-on-active/model-8f534a8c"
+
+# Make the memoize directory
+mkdir -p $PREP_MEMOIZE_DIR
+
+# Set tmp directory
+export TMPDIR="/tmp"
+
+# Activate adapt conda environment
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate adapt-prod
+
+mkdir -p out/designs
+
+# Run the design
+/usr/bin/time -f "mem=%K RSS=%M elapsed=%e cpu.sys=%S .user=%U" design.py complete-targets auto-from-file $IN_TSV $OUT_TSV_DIR -gl $ARG_GL -gm $ARG_GM -gp $ARG_GP -pl $ARG_PL -pm $ARG_PM -pp $ARG_PP --id-m 4 --id-frac 0.01 --require-flanking3 H --max-primers-at-site $ARG_MAXPRIMERSATSITE --primer-gc-content-bounds 0.4 0.6 --max-target-length $ARG_MAXTARGETLENGTH --cost-fn-weights $ARG_COSTFNWEIGHTS --best-n-targets $ARG_BESTNTARGETS --predict-activity-model-path $PREDICTIVE_MODEL --mafft-path $MAFFT_PATH --prep-memoize-dir $PREP_MEMOIZE_DIR --cluster-threshold $CLUSTER_THRESHOLD --use-fasta $MANUAL_SEQ_INPUT --only-design-for input/only-design.tsv --write-input-seqs --write-input-aln --verbose &> out/design.out
+
+# gzip the stdout/stderr
+gzip out/design.out
