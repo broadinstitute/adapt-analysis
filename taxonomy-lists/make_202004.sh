c85f78d330d93d2bcb5dcd71c07ba76b05dc0f99
Hayden Metsky <hmetsky@gmail.com>
2020-04-29 20:54:10 -0400

    Add list of vertebrate-associated viral species parsed from NCBI accession list

---
+++ taxonomy-lists/make_202004.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python scripts/select_taxa_from_accession_list.py 2020-04/taxid10239.20200429.with-additions.nbr.gz 2020-04/taxa.tsv --force-include-taxa 2020-04/taxa-to-force-include.tsv  --force-exclude-taxa 2020-04/taxa-to-exclude.tsv
