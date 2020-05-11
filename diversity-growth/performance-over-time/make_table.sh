2826818de1460cf9942c7790f55a4b2fa9851807
Hayden Metsky <hmetsky@gmail.com>
2020-05-11 13:18:08 -0400

    Run performance-over-time analysis on influenza subtypes

--- diversity-growth/performance-over-time/make_table.sh
+++ diversity-growth/performance-over-time/make_table.sh
@@ -4,8 +4,13 @@
 # in different years, against sequences from future years.
 
 # Allow activating conda environments
-source ~/anaconda/etc/profile.d/conda.sh
+source ~/anaconda3/etc/profile.d/conda.sh
 
 # Activate adapt
 conda activate adapt
 
+for subtype in H1Nany H3Nany HanyN1 HanyN2; do
+    for design_year in $(seq 2007 2019); do
+        python -u pick_kmers_and_compute_coverage.py --in-fasta iav-${subtype}/in/iav-${subtype}.2005-present.aligned.fasta.gz --out-tsv iav-${subtype}/by-year/designs.${design_year}.tsv --mismatches 1 --design-year $design_year --years-included-in-design 3 &> iav-${subtype}/by-year/designs.${design_year}.out &
+    done
+done
--- diversity-growth/performance-over-time/pick_kmers_and_compute_coverage.py
+++ diversity-growth/performance-over-time/pick_kmers_and_compute_coverage.py
@@ -5,6 +5,7 @@ sequences from each year.
 
 import argparse
 from collections import defaultdict
+import statistics
 import random
 import re
 
@@ -44,20 +45,50 @@ def fetch_collection_dates(accessions, batch_size=100, reqs_per_sec=2):
     return collection_dates
 
 
-def read_sequences(fn):
+def parse_influenza_collection_dates(headers):
+    """Parse collection date from influenza sequence header.
+
+    The date is the third to last field, space-separated.
+
+    Args:
+        headers: collection of sequence headers
+
+    Returns:
+        dict {accession: str of collection date}
+    """
+    collection_dates = {}
+    for header in headers:
+        hs = header.split(' ')
+        accver = hs[0]
+        acc = accver.split('.')[0]
+        date = hs[-3]
+        collection_dates[acc] = date
+    return collection_dates
+
+
+def read_sequences(fn, influenza=True):
     """Read alignment and fetch collection dates.
 
     Args:
         fn: path to alignment of sequences; sequence headers
             contain accession
+        influenza: if True, assume sequence headers are from NCBI influenza
+            database
 
     Returns:
         tuple (Alignment object, dict {sequence index in alignment: year})
     """
     seqs = seq_io.read_fasta(fn)
 
-    accs = [h.split(' ')[0] for h in seqs.keys()]
-    collection_dates = fetch_collection_dates(accs)
+    if influenza:
+        collection_dates = parse_influenza_collection_dates(seqs.keys())
+        # Below assumes a collection of possible dates, so transform the
+        # dict
+        collection_dates = {acc: [date] for acc, date in
+                collection_dates.items()}
+    else:
+        accs = [h.split(' ')[0] for h in seqs.keys()]
+        collection_dates = fetch_collection_dates(accs)
 
     seq_list = []
     i = 0
@@ -94,7 +125,7 @@ def seq_idxs_in_year_range(years, start, end=None):
 
     Args:
         years: dict {sequence index: year}
-        start/end: start (inclusive) and end (exclusive) year; if
+        start/end: start (inclusive) and end (inclusove) year; if
             end is None, only pick ones from start year; if start
             is None, pick all sequences up to end
 
@@ -107,10 +138,10 @@ def seq_idxs_in_year_range(years, start, end=None):
             if year == start:
                 idxs.add(idx)
         elif start is None:
-            if year < end:
+            if year <= end:
                 idxs.add(idx)
         else:
-            if start <= year < end:
+            if start <= year <= end:
                 idxs.add(idx)
     return idxs
 
@@ -135,16 +166,20 @@ def bootstrap_alignment(aln):
     return alignment.Alignment.from_list_of_seqs(seq_list_resampled)
 
 
-def find_mode_kmer(aln, k=28):
-    """Find most common k-mer across alignment -- the one found in
+def find_mode_kmers(aln, k=30, num_kmers=15):
+    """Find most common k-mers across alignment -- the ones found in
     the highest fraction of sequences.
+
+    This requires that k-mers come from different sites (so only allows the
+    most common one at each site) and, moreover, that they do not overlap.
 
     Args:
         aln: alignment.Alignment object
         k: k-mer length
+        num_kmers: number of common k-mers to return
 
     Returns:
-        k-mer that is mode across alignment
+        list of k-mers that are most common across alignment
     """
     kmers_with_frac = []
     for pos in range(0, aln.seq_length - k + 1):
@@ -167,14 +202,28 @@ def find_mode_kmer(aln, k=28):
                 mode_kmer, 0, 0, False)
         mode_kmer_frac = float(len(mode_kmer_bound)) / aln_for_kmer.num_sequences
 
-        kmers_with_frac += [(mode_kmer, mode_kmer_frac)]
+        kmers_with_frac += [(mode_kmer, mode_kmer_frac, pos)]
 
-    # Select the k-mer overall with the highest fraction bound
-    best = max(kmers_with_frac, key=lambda x: x[1])
-    return best[0]
+    # Select the k-mers with the highest fraction bound
+    selected_kmers = []
+    best = sorted(kmers_with_frac, key=lambda x: x[1], reverse=True)
+    for kmer, frac, pos in best:
+        if len(selected_kmers) == num_kmers:
+            break
+        # Check if kmer overlaps with any in selected_kmers
+        overlaps = False
+        for other_kmer, other_kmer_pos in selected_kmers:
+            if (pos <= other_kmer_pos + k and
+                    other_kmer_pos <= pos + k):
+                # kmer overlaps with other_kmer
+                overlaps = True
+                break
+        if not overlaps:
+            selected_kmers += [(kmer, pos)]
+    return [x[0] for x in selected_kmers]
 
 
-def find_mode_kmers_bootstrap(aln, k=28, sample_size=10):
+def find_mode_kmers_bootstrap(aln, k=30, sample_size=10):
     """Find mode k-mers over bootstrap samples of the alignment.
 
     Args:
@@ -183,15 +232,15 @@ def find_mode_kmers_bootstrap(aln, k=28, sample_size=10):
         sample_size: number of bootstrap samples
 
     Returns:
-        list of sample_size k-mers that are each mode across alignment
+        list of sample_size lists of k-mers that are each mode across alignment
         for the bootstrap
     """
-    mode_kmers = []
+    mode_kmers_bootstrap = []
     for _ in range(sample_size):
         aln_bootstrap = bootstrap_alignment(aln)
-        mode_kmer = find_mode_kmer(aln_bootstrap, k=k)
-        mode_kmers += [mode_kmer]
-    return mode_kmers
+        mode_kmers = find_mode_kmers(aln_bootstrap, k=k)
+        mode_kmers_bootstrap += [mode_kmers]
+    return mode_kmers_bootstrap
 
 
 def extract_aln_with_seq_idxs(aln, seq_idxs):
@@ -208,53 +257,62 @@ def extract_aln_with_seq_idxs(aln, seq_idxs):
     return alignment.Alignment.from_list_of_seqs(seq_list)
 
 
-def make_designs_from_each_year(aln, years, start_year, end_year):
-    """Produce "designs" from eahc year.
+def make_designs_from_each_year(aln, years, start_year, end_year,
+        included_years):
+    """Produce "designs" for each year.
 
     This chooses k-mers (one per bootstrap sample) using all sequences
-    collected up to an including each year.
+    collected recently for each year.
 
     Args:
         aln: alignment.Alignment object
         years: dict {sequence index: year}
         start_year/end_year: start year (inclusive) and end year
             (inclusive) to consider
+        included_years: number of years to include in the design; for a
+            year Y, this designs using sequence data from years
+            (Y - included_years + 1) through Y
 
     Returns:
-        dict {year: list of k-mers (one per bootstrap)}
+        dict {year: list of lists x_i of k-mers (one x_i per bootstrap)}
     """
+    assert included_years >= 1
+
     # Choose k-mers for each year
     kmers_for_year = {}
     for year in range(start_year, end_year + 1):
         # Find all sequences collected in years up to and
         # including year
         print('Finding k-mers for year %d' % year)
-        seq_idxs = seq_idxs_in_year_range(years, start=None, end=year+1)
+        design_start_year = year - included_years + 1
+        seq_idxs = seq_idxs_in_year_range(years, start=design_start_year, end=year)
         aln_year = extract_aln_with_seq_idxs(aln, seq_idxs)
 
         kmers_for_year[year] = find_mode_kmers_bootstrap(aln_year)
     return kmers_for_year
 
 
-def compute_fraction_hit(aln, years, start_year, end_year, kmer, mismatches):
-    """Find fraction of sequences hit by a k-mer in each year.
+def compute_fraction_hit(aln, years, start_year, end_year, kmers, mismatches):
+    """Find mean fraction of sequences hit by k-mers in each year.
 
     This tolerates mismatches between k-mer and target, but explicitly
     does not allow G-U pairing.
+
+    The mean is calculated over the k-mers.
 
     Args:
         aln: alignment.Alignment object
         years: dict {sequence index: year}
         start_year/end_year: start year (inclusive) and end year
             (inclusive) to consider
-        kmer: k-mer to query
+        kmers: list of k-mers to query
         mismatches: number of mismatches to tolerate when determining
             if a k-mer 'hits' a target sequence
 
     Returns:
-        dict {year: fraction hit of sequences in year hit}
+        dict {year: mean fraction hit of sequences in year hit}
     """
-    k = len(kmer)
+    k = len(kmers[0])
 
     frac_hit_in_year = {}
     for year in range(start_year, end_year + 1):
@@ -263,18 +321,23 @@ def compute_fraction_hit(aln, years, start_year, end_year, kmer, mismatches):
         aln_year = extract_aln_with_seq_idxs(aln, seq_idxs)
 
         # Scan along the alignment to find the site with
-        # the most sequences bound (we don't have the site
+        # the most sequences bound for each k-mer (we don't have the site
         # where k-mer binds to, so we have to find it)
-        fracs_bound = []
+        fracs_bound = defaultdict(list)
         for pos in range(0, aln.seq_length - k + 1):
             aln_at_pos = aln_year.extract_range(pos, pos + k)
 
-            kmer_bound = aln_at_pos.sequences_bound_by_guide(
-                    kmer, 0, mismatches, False)
-            frac = float(len(kmer_bound)) / aln_year.num_sequences
-            fracs_bound += [frac]
-        frac_bound = max(fracs_bound)
-        frac_hit_in_year[year] = frac_bound
+            for kmer in kmers:
+                kmer_bound = aln_at_pos.sequences_bound_by_guide(
+                        kmer, 0, mismatches, False)
+                frac = float(len(kmer_bound)) / aln_year.num_sequences
+                fracs_bound[kmer].append(frac)
+
+        frac_bound_over_kmers = []
+        for kmer in kmers:
+            frac_bound = max(fracs_bound[kmer])
+            frac_bound_over_kmers += [frac_bound]
+        frac_hit_in_year[year] = statistics.mean(frac_bound_over_kmers)
 
     return frac_hit_in_year
 
@@ -285,8 +348,12 @@ def main(args):
     end_year = max(years.values())
 
     # Produce designs
+    if args.design_year:
+        design_start_year, design_end_year = args.design_year, args.design_year
+    else:
+        design_start_year, design_end_year = start_year, end_year
     kmers_for_year = make_designs_from_each_year(aln, years,
-            start_year, end_year)
+            design_start_year, design_end_year, args.years_included_in_design)
 
     # Determine fraction of sequences from each year that
     # are hit by each k-mer, and write this to TSV file
@@ -294,17 +361,19 @@ def main(args):
         def write_row(row):
             fw.write('\t'.join(str(x) for x in row) + '\n')
 
-        header = ['design_year', 'sampling', 'kmer', 'test_year',
+        header = ['design_year', 'sampling', 'kmers', 'test_year',
                 'frac_hit']
         write_row(header)
 
         for design_year, kmers in kmers_for_year.items():
-            for sample_i, kmer in enumerate(kmers):
+            for sample_i, sample_kmers in enumerate(kmers):
                 frac_hit_in_year = compute_fraction_hit(
-                        aln, years, start_year, end_year, kmer,
+                        aln, years, start_year, end_year, sample_kmers,
                         args.mismatches)
+                sample_kmers_str = ','.join(sample_kmers)
                 for test_year, frac_hit in frac_hit_in_year.items():
-                    row = [design_year, sample_i, kmer, test_year, frac_hit]
+                    row = [design_year, sample_i, sample_kmers_str,
+                            test_year, frac_hit]
                     write_row(row)
 
 
@@ -321,6 +390,16 @@ if __name__ == "__main__":
         default=1,
         help=("Number of mismatches when determining whether a k-mer "
               "hits/detects a target sequence"))
+    parser.add_argument('--design-year',
+        type=int,
+        help=("If set, only produce and evaluate designs produced in this "
+              "year"))
+    parser.add_argument('--years-included-in-design',
+        type=int,
+        default=2,
+        help=("Number of years to include in a design. When designing for "
+              "a year Y, this uses sequence data from all years in [Y - "
+              "YEARS_INCLUDED_IN_DESIGN + 1, Y]."))
 
     args = parser.parse_args()
     main(args)
