d6385ea40aeef822f5c294a2bb1e790a8a1bb0dc
Hayden Metsky <hmetsky@gmail.com>
2020-05-20 22:25:25 -0400

    Update outbreak-entropy plot

--- diversity-growth/outbreak-entropy/plot.R
+++ diversity-growth/outbreak-entropy/plot.R
@@ -27,16 +27,24 @@ entropy$window.mid <- entropy$window.start + (entropy$window.end - entropy$windo
 entropy <- entropy[entropy$window.start != min(entropy$window.start),]
 entropy <- entropy[entropy$window.start != max(entropy$window.start),]
 
-# Make a hard cutoff on entropy of 0.3 to avoid issues from outliers
-#entropy$entropy[entropy$entropy > 0.3] <- 0.3
+# Make a hard cutoff on entropy of 1.25 to limit a few outliers from taking over
+entropy$entropy[entropy$entropy > 1.25] <- 1.25
+
+# Add column giving month/day only -- e.g., 'Feb. 1' instead of '2020-02-01'
+# Keep the levels sorted in the right order (i.e., as given in start.date)
+entropy$start.date.simple <- factor(entropy$start.date)
+levels(entropy$start.date.simple) <- format(as.Date(levels(entropy$start.date.simple)), "%b %e")
+
+# Manually specify breaks in legend
+breaks <- c(0.0, 0.5, 1.0)
 
 # Make a heatmap
-p <- ggplot(entropy, aes(y=start.date, x=window.mid, fill=entropy)) +
+p <- ggplot(entropy, aes(y=start.date.simple, x=window.mid, fill=entropy)) +
         geom_tile() +
         theme_pubr() +
-        scale_y_discrete(limits = rev(levels(entropy$start.date))) +
-        scale_fill_gradient(low="white", high="#3E1451") +
+        scale_y_discrete(limits = rev(levels(entropy$start.date.simple))) +
+        scale_fill_gradient(low="white", high="#3E1451", breaks=breaks, name="Entropy") +
         xlab("Genome") +
         ylab("Date of sample collection")
 
-ggsave(OUT.PDF, p, device="pdf", width=16, height=8, useDingbats=FALSE)
+ggsave(OUT.PDF, p, device="pdf", width=16, height=4, useDingbats=FALSE)
