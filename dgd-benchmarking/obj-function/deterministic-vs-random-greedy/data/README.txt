0a5f716e1597915b73d83149c82e50e2e0c0668a
Hayden Metsky <hmetsky@gmail.com>
2020-06-01 00:29:40 -0400

    Add Lassa to obj-function algorithm comparison

--- dgd-benchmarking/obj-function/deterministic-vs-random-greedy/data/README.txt
+++ dgd-benchmarking/obj-function/deterministic-vs-random-greedy/data/README.txt
@@ -6,3 +6,7 @@ way.
   - For sars-related-cov, this used `auto-from-args 694009 None NC_045512,NC_004718`,
     and also `--subsample-seqs 250` to limit the input
   - For rhinovirus-a, this used `auto-from-args 147711 None NC_038311,NC_001617`
+  - For lassa-S, this used `11620 S KM821998,GU481072,KM821773`
+
+Example command:
+  design.py sliding-window auto-from-args 11620 S KM821998,GU481072,KM821773 /dev/null -gl 28 --mafft-path ~/viral-ngs/viral-ngs-etc/conda-env/bin/mafft --write-input-aln lassa-S.fasta --verbose
--- dgd-benchmarking/obj-function/deterministic-vs-random-greedy/run.sh
+++ dgd-benchmarking/obj-function/deterministic-vs-random-greedy/run.sh
@@ -15,26 +15,19 @@ echo "Making designs"
 # Try different values for soft and hard guide constraints, and
 # for the penalty strength
 # Since there is randomness, do this all 5 times
-# Try 2 taxa: sars-related-cov should work with 1-2 guides, and
-# rhinovirus-a has a lot of diversity and may require many guides
+# Try 3 taxa: sars-related-cov should work with 1-2 guides, and
+# rhinovirus-a has a lot of diversity and may require many guides,
+# as well as lassa-S
 echo -n "" > $commandsfile
 for i in $(seq 1 5); do
     for hgc in $(seq 1 4); do
         for ((sgc=1; sgc<=hgc; sgc++)); do
             for ps in 0.1 0.5 1.0; do
                 for algo in greedy random-greedy; do
-                    for tax in sars-related-cov rhinovirus-a; do
-                        if [[ "$tax" == "sars-related-cov" ]]; then
-                            primer_gc_bounds="0.4 0.6"
-                            pm="2"
-                        fi
-                        if [[ "$tax" == "rhinovirus-a" ]]; then
-                            primer_gc_bounds="0.3 0.7"
-                            pm="3"
-                        fi
+                    for tax in sars-related-cov rhinovirus-a lassa-S; do
                         outf="designs.${tax}_${algo}_${hgc}_${sgc}_${ps}_${i}.tsv"
                         if [ ! -f "designs/$outf" ]; then
-                            cmd="design.py complete-targets fasta data/${tax}.fasta.gz -o designs/$outf -pm $pm -pp 0.9 --primer-gc-content-bounds $primer_gc_bounds --max-primers-at-site 10 -gl 28 --max-target-len 250 --best-n-targets 10 --predict-activity-model-path $ADAPT_PATH/models/classify/model-51373185 $ADAPT_PATH/models/regress/model-f8b6fd5d --obj maximize-activity --soft-guide-constraint $sgc --hard-guide-constraint $hgc --penalty-strength $ps --maximization-algorithm $algo --verbose &> out/$outf.out"
+                            cmd="design.py complete-targets fasta data/${tax}.fasta.gz -o designs/$outf -pm 3 -pp 0.9 --primer-gc-content-bounds 0.3 0.7 --max-primers-at-site 10 -gl 28 --max-target-len 250 --best-n-targets 10 --predict-activity-model-path $ADAPT_PATH/models/classify/model-51373185 $ADAPT_PATH/models/regress/model-f8b6fd5d --obj maximize-activity --soft-guide-constraint $sgc --hard-guide-constraint $hgc --penalty-strength $ps --maximization-algorithm $algo --verbose &> out/$outf.out"
                             echo "$cmd" >> $commandsfile
                         fi
                     done
@@ -45,7 +38,7 @@ for i in $(seq 1 5); do
 done
 
 # Run commands in parallel
-parallel --jobs 72 --no-notice < $commandsfile
+parallel --jobs 24 --no-notice < $commandsfile
 
 rm $commandsfile
 
@@ -60,7 +53,7 @@ for i in $(seq 1 5); do
         for ((sgc=1; sgc<=hgc; sgc++)); do
             for ps in 0.1 0.5 1.0; do
                 for algo in greedy random-greedy; do
-                    for tax in sars-related-cov rhinovirus-a; do
+                    for tax in sars-related-cov rhinovirus-a lassa-S; do
                         designfile="designs/designs.${tax}_${algo}_${hgc}_${sgc}_${ps}_${i}.tsv"
                         # Pull out the columns we want for the first 5 rows (excluding header)
                         cat $designfile | tail -n +2 | head -n 5 | awk -F'\t' -v i="$i" -v hgc="$hgc" -v sgc="$sgc" -v ps="$ps" -v algo="$algo" -v tax="$tax" '{print i"\t"hgc"\t"sgc"\t"ps"\t"algo"\t"tax"\t"$1"\t"$13"\t"$15}' >> $summaryfile
