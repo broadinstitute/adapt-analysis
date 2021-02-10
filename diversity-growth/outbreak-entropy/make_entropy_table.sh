85df2e913c094454e2ebcb786135bfd9ec8f8314
Hayden Metsky <hmetsky@gmail.com>
2021-02-10 10:28:07 -0500

    Update SARS-CoV-2 variants plot with more recent data

--- diversity-growth/outbreak-entropy/make_entropy_table.sh
+++ diversity-growth/outbreak-entropy/make_entropy_table.sh
@@ -9,6 +9,6 @@ source ~/anaconda3/etc/profile.d/conda.sh
 # Activate adapt
 conda activate adapt
 
-python compute_entropy_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2020-11-12/msa_1112.fasta.gz --out-tsv sars-cov-2/entropy.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2020-11-01 --interval-days 7 --window-length 500 --window-stride 25 -k 500 --sample-size-per-date-interval -1
+python compute_entropy_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2021-02-01/msa_0201.fasta.gz --out-tsv sars-cov-2/entropy.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2021-01-01 --interval-days 7 --window-length 500 --window-stride 25 -k 500 --sample-size-per-date-interval -1
 
 gzip sars-cov-2/entropy.tsv
--- diversity-growth/outbreak-entropy/make_variants_table.sh
+++ diversity-growth/outbreak-entropy/make_variants_table.sh
@@ -9,10 +9,10 @@ source ~/anaconda3/etc/profile.d/conda.sh
 # Activate adapt
 conda activate adapt
 
-python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2020-11-12/msa_1112.fasta.gz --out-tsv sars-cov-2/variants.no-subsample.no-cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2020-11-01 --interval-by-month --sample-size-per-date-interval -1
-python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2020-11-12/msa_1112.fasta.gz --out-tsv sars-cov-2/variants.no-subsample.cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2020-11-01 --interval-by-month --sample-size-per-date-interval -1 --cumulative
-python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2020-11-12/msa_1112.fasta.gz --out-tsv sars-cov-2/variants.subsample-300.no-cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2020-11-01 --interval-by-month --sample-size-per-date-interval 300
-python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2020-11-12/msa_1112.fasta.gz --out-tsv sars-cov-2/variants.subsample-300.cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2020-11-01 --interval-by-month --sample-size-per-date-interval 300 --cumulative
+python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2021-02-01/msa_0201.fasta.gz --out-tsv sars-cov-2/variants.no-subsample.no-cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2021-01-01 --interval-by-month --sample-size-per-date-interval -1
+python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2021-02-01/msa_0201.fasta.gz --out-tsv sars-cov-2/variants.no-subsample.cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2021-01-01 --interval-by-month --sample-size-per-date-interval -1 --cumulative
+python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2021-02-01/msa_0201.fasta.gz --out-tsv sars-cov-2/variants.subsample-300.no-cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2021-01-01 --interval-by-month --sample-size-per-date-interval 300
+python compute_variants_over_time.py --in-fasta sars-cov-2/in/gisaid_msa_2021-02-01/msa_0201.fasta.gz --out-tsv sars-cov-2/variants.subsample-300.cumulative.tsv --ref-acc 'EPI_ISL_402119' --start-date 2020-02-01 --end-date 2021-01-01 --interval-by-month --sample-size-per-date-interval 300 --cumulative
 
 gzip sars-cov-2/variants.no-subsample.no-cumulative.tsv
 gzip sars-cov-2/variants.no-subsample.cumulative.tsv
