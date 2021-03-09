19ee002735bcba0d26246aadba1c71470f01a602
Hayden Metsky <hmetsky@gmail.com>
2021-03-09 15:24:02 -0500

    Update analysis of original 2019-nCoV (SARS-CoV-2) assay against genomes up to 2021-03-04

--- misc-designs/ncov/evaluate-original-design/evaluate.sh
+++ misc-designs/ncov/evaluate-original-design/evaluate.sh
@@ -5,7 +5,7 @@
 # ADAPT must be loaded (conda env) before running.
 
 ADAPT_PATH="$HOME/adapt"
-GENOMES_PATH="./data/gisaid_msa_2020-11-12/msa_1112.fasta.gz"
+GENOMES_PATH="./data/gisaid_msa_2021-03-04/msa_0304.fasta.gz"
 
 # Determine active fraction
 analyze_coverage.py sars-cov-2.v1-experimentally-tested.tsv $GENOMES_PATH -pm 2 --write-frac-bound frac-bound.active.tsv --verbose --predict-activity-model-path $ADAPT_PATH/models/classify/model-51373185 $ADAPT_PATH/models/regress/model-f8b6fd5d
