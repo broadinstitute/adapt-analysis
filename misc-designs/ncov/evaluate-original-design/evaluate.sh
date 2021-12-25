6fd0ab0e35c21b2b2ce7d56a62c49862d6eecc9b
Hayden Metsky <hmetsky@gmail.com>
2021-12-25 11:07:17 -0500

    Update evaluation of original SARS-CoV-2 assay to account for genomes up to December 12 2021

--- misc-designs/ncov/evaluate-original-design/evaluate.sh
+++ misc-designs/ncov/evaluate-original-design/evaluate.sh
@@ -5,10 +5,10 @@
 # ADAPT must be loaded (conda env) before running.
 
 ADAPT_PATH="$HOME/adapt"
-GENOMES_PATH="./data/gisaid_msa_2021-03-04/msa_0304.fasta.gz"
+GENOMES_PATH="./data/gisaid_msa_2021-12-12/msa_1212.fasta.gz"
 
 # Determine active fraction
-analyze_coverage.py sars-cov-2.v1-experimentally-tested.tsv $GENOMES_PATH -pm 2 --write-frac-bound frac-bound.active.tsv --verbose --predict-activity-model-path $ADAPT_PATH/models/classify/model-51373185 $ADAPT_PATH/models/regress/model-f8b6fd5d
+analyze_coverage.py sars-cov-2.v1-experimentally-tested.tsv $GENOMES_PATH -pm 2 --write-frac-bound frac-bound.active.tsv --verbose --predict-activity-model-path $ADAPT_PATH/adapt/models/classify/cas13a/v1_0 $ADAPT_PATH/adapt/models/regress/cas13a/v1_0
 
 # Determine highly active fraction
-analyze_coverage.py sars-cov-2.v1-experimentally-tested.tsv $GENOMES_PATH -pm 2 --write-frac-bound frac-bound.highly-active.tsv --verbose --predict-activity-model-path $ADAPT_PATH/models/classify/model-51373185 $ADAPT_PATH/models/regress/model-f8b6fd5d --predict-activity-require-highly-active
+analyze_coverage.py sars-cov-2.v1-experimentally-tested.tsv $GENOMES_PATH -pm 2 --write-frac-bound frac-bound.highly-active.tsv --verbose --predict-activity-model-path $ADAPT_PATH/adapt/models/classify/cas13a/v1_0 $ADAPT_PATH/adapt/models/regress/cas13a/v1_0 --predict-activity-require-highly-active
